4ee19a466614cc8e8c237db89655c2b8bff595ec
Tair <obra-aleksandr@yandex.ru>
2017-03-21 12:22:12 +0300

    Поиск товара

--- config/web.php
+++ config/web.php
@@ -47,6 +47,7 @@ $config = [
             'rules' => [
                 'category/<id:\d+>/page/<page:\d+>' => 'category/view',
                 'category/<id:\d+>' => 'category/view',
+                'search' => 'category/search',
                 'product/<id:\d+>' => 'product/view',
             ],
         ],
--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -9,6 +9,7 @@
 namespace app\controllers;
 use app\models\Category;
 use app\models\Product;
+use yii\web\HttpException;
 use Yii;
 
 
@@ -17,6 +18,8 @@ class ProductController extends AppController
     public function actionView($id){
         $id = Yii::$app->request->get('id');
         $product = Product::findOne($id);
+        if (empty($product))
+            throw new HttpException('404', 'Такого товара нет');
         /*$product = Product::find()->with('Category')->where(['id' => $id])->limit(1)->one();*/
         $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
         $this->setMeta('E-SHOPPER | '. $product->name, $product->keywords, $product->description);
